ce1efffaba859dae2d3dd1bd94552dbd58df0ccc
KisSsArt <59938902+KisSsArt@users.noreply.github.com>
2024-07-28 01:43:26 +0300

    move http.h to http namespace

--- HttpData.cpp
+++ HttpData.cpp
@@ -1,5 +1,9 @@
 #include "HttpData.h"
 
+#include <fstream>
+
+using namespace http;
+
 HTTPData* HTTPData::ParsePacket(std::string packet)
 {
     size_t currentPos;
@@ -77,4 +81,32 @@ HTTPData* HTTPData::ParsePacket(std::string packet)
     }
 
     return this;
+}
+
+char* HTTPData::CreateResponsePacket()
+{
+    char* response = new char[1024];
+    std::memset(response, '\0', 1024);
+
+    if (!this->getValid())
+        return CreateResponseHeader(response, this->getHTTPVersion(), HTTP_BAD_REQUEST);
+
+    std::ifstream file(this->getFile());
+    if (!file.is_open())
+        return CreateResponseHeader(response, this->getHTTPVersion(), HTTP_NOT_FOUND);
+
+    CreateResponseHeader(response, this->getHTTPVersion(), HTTP_OK);
+    file.read(&(response[std::strlen(response)]), (1024 - std::strlen(response)));
+    file.close();
+    return response;
+}
+
+char* HTTPData::CreateResponseHeader(char* response, HTTP_ver version, HTTP_STATUS status)
+{
+    std::string httpHeader(httpEmptyHeader);
+    httpHeader.insert(5, version==HTTP_10 ? "1.0":"1.1");
+    httpHeader.insert(9, statusStringMap[status]);
+
+    std::strcpy(response, httpHeader.c_str());
+    return response;
 }
--- HttpData.h
+++ HttpData.h
@@ -1,30 +1,83 @@
 #pragma once
 
 #include <string>
-#include "http.h"
+#include <map>
+
 #include "config.h"
 
-class HTTPData {
-public:
-    HTTPData() : valid{true},method{GET},file{DEFAULT_FILE_NAME},version{HTTP_10} {} 
+namespace http
+{
+    inline std::string httpEmptyHeader =  "HTTP/ \r\n"
+                                    "Content-Type: text/html\r\n"
+                                    "\r\n";
 
-    HTTPData(std::string packet) { this->setValid(true); this->ParsePacket(packet); }
+    /*
+        HTTP status codes
+    */
+    enum HTTP_STATUS : int {
+        // 1xx: Info
 
-    HTTPData* ParsePacket(std::string packet);
+        // 2xx: Success
+        HTTP_OK = 200,
+        // 3xx: Redirection
 
-    bool getValid() { return this->valid; }
-    HTTP_met getHTTPMethod() { return this->method; }
-    std::string getFile() { return this->file; }
-    HTTP_ver getHTTPVersion() { return this->version; }
+        // 4xx: Client Error
+        HTTP_BAD_REQUEST = 400,
+        HTTP_NOT_FOUND = 404,
+        // 5xx: Server Error
+        HTTP_VERSION_NOT_SUPPORTED = 505,
+    };
 
-    void setValid(bool valid) { this->valid = valid; }
-    void setHTTPMethod(HTTP_met method) { this->method = method; }
-    void setFile(std::string file) { this->file = file; }
-    void setHTTPVersion(HTTP_ver version) { this->version = version; }
+    inline std::map<HTTP_STATUS, std::string> statusStringMap{
+        {HTTP_OK, "200 OK"},
+        {HTTP_BAD_REQUEST, "400 Bad Request"},
+        {HTTP_NOT_FOUND, "404 Not Found"},
+    };
 
-private:
-    bool valid;
-    HTTP_met method;
-    std::string file;
-    HTTP_ver version;
-};
+    /*
+        HTTP versions
+    */
+    enum HTTP_ver : int {
+        HTTP_10 = 0,
+        HTTP_11 = 1
+    };
+
+    /*
+        HTTP method
+    */
+    enum HTTP_met : int {
+        GET = 0,
+        POST = 1
+    };
+
+    class HTTPData {
+    public:
+        HTTPData() : valid{true}, method{GET}, file{DEFAULT_FILE_NAME}, version{HTTP_10} {}
+
+        HTTPData(std::string packet)
+        {
+            this->setValid(true);
+            this->ParsePacket(packet);
+        }
+
+        HTTPData* ParsePacket(std::string packet);
+        char* CreateResponsePacket();
+        char* CreateResponseHeader(char* response, HTTP_ver version, HTTP_STATUS status);
+
+        bool getValid() { return this->valid; }
+        HTTP_met getHTTPMethod() { return this->method; }
+        std::string getFile() { return this->file; }
+        HTTP_ver getHTTPVersion() { return this->version; }
+
+        void setValid(bool valid) { this->valid = valid; }
+        void setHTTPMethod(HTTP_met method) { this->method = method; }
+        void setFile(std::string file) { this->file = file; }
+        void setHTTPVersion(HTTP_ver version) { this->version = version; }
+
+    private:
+        bool valid;
+        HTTP_met method;
+        std::string file;
+        HTTP_ver version;
+    };
+}
